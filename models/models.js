b22feab1529dae590f46ac5c092c02b5239efc54
Tomoneill98 <oneillt98@gmail.com>
2023-05-11 12:05:57 +0100

    getting back to 0

--- models/models.js
+++ models/models.js
@@ -13,27 +13,17 @@ exports.fetchAPIsData = () => {
   });
 };
 
-
-//task 6
-exports.fetchCommentsById = (article_id) => {
-  return connection
-    .query(
-      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`,
-      [article_id]
-    )
-    .then((result) => {
-      if (result.rows) return result.rows;
-
-
 exports.fetchArticles = () => {
   return connection
     .query(
       `SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id
-      GROUP BY articles.article_id
-      ORDER BY articles.created_at DESC;`
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;`
     )
     .then((result) => {
       return result.rows;
+    });
+};
 
 exports.fetchArticlesById = (article_id) => {
   return connection
@@ -46,6 +36,17 @@ exports.fetchArticlesById = (article_id) => {
         });
       }
       return result.rows[0];
+    });
+};
 
+//task 6
+exports.fetchCommentsById = (article_id) => {
+  return connection
+    .query(
+      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`,
+      [article_id]
+    )
+    .then((result) => {
+      if (result.rows) return result.rows;
     });
 };
